60061afe89849ebd9dd55c57495b825aa76d5bb3
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-16 21:06:50 -0300

    Atualização do exercício

---
+++ ex007/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final = 50;
+            string resultado;
+
+            if (nota_final >= 60)
+            {
+                resultado = "aprovado";
+            }
+            else
+            {
+                resultado = "reprovado";
+            }
+
+            Console.WriteLine("O aluno foi {0}!!", resultado);
+        }
+    }
+}
