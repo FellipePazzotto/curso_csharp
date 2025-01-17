0905ed021369850f5d3244880d70480e63855003
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-16 21:57:16 -0300

    Adicionando entradas para notas bimestrais

--- ex007/Program.cs
+++ ex007/Program.cs
@@ -4,11 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite a nota final: ");
-            double nota_final = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a primeira nota: ");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("--------------------------------------------------");
 
+            Console.Write("Digite a segunda nota: ");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.Write("Digite a terceira nota: ");
+            double nota3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.Write("Digite a quarta nota: ");
+            double nota4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------");
+
+            double nota_final = (nota1 + nota2 + nota3 + nota3) / 4;
             string resultado;
+
             if (nota_final >= 60)
             {
                 if (nota_final >= 90)
@@ -31,7 +45,7 @@
                     resultado = "reprovado..."; 
                 }
             }
-            Console.WriteLine("Com a nota de {0}, o aluno está {1}", 
+            Console.WriteLine("A média é {0} o aluno está {1}", 
                 nota_final ,resultado);
         }
     }
