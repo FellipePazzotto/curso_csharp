d51dd997cfd0240a22fdade07e49f081559cb3ec
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-16 21:48:51 -0300

    Estruturando as condicionais

--- ex007/Program.cs
+++ ex007/Program.cs
@@ -9,18 +9,28 @@
 
             if (nota_final >= 60)
             {
-                resultado = "aprovado!!";
-            }
-            else if (nota_final >= 40)
-            {
-                resultado = "de recuperação.";
+                if (nota_final >= 90)
+                {
+                    resultado = "aprovado com mérito, parabéns!!";
+                }
+                else
+                {
+                    resultado = "aprovado!!";
+                }
             }
             else
             {
-                resultado = "reprovado...";
+                if (nota_final >= 40)
+                {
+                    resultado = "de recuperação.";
+                }
+                else
+                {
+                    resultado = "reprovado..."; 
+                }
             }
-
-            Console.WriteLine("O aluno está {0}", resultado);
+            Console.WriteLine("Com a nota de {0}, aluno está {1}", 
+                nota_final ,resultado);
         }
     }
 }
