44369c3a06decb1988d314dcc67b5db0e70b0444
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-16 21:23:12 -0300

    Adicionando outra condicional

--- ex007/Program.cs
+++ ex007/Program.cs
@@ -4,19 +4,23 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 50;
+            double nota_final = 30;
             string resultado;
 
             if (nota_final >= 60)
             {
-                resultado = "aprovado";
+                resultado = "aprovado!!";
+            }
+            else if (nota_final >= 40)
+            {
+                resultado = "de recuperação.";
             }
             else
             {
-                resultado = "reprovado";
+                resultado = "reprovado...";
             }
 
-            Console.WriteLine("O aluno foi {0}!!", resultado);
+            Console.WriteLine("O aluno está {0}", resultado);
         }
     }
 }
