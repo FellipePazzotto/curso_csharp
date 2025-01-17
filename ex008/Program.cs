b4a2a85dbc8a486230ce2c3ed87b83a2516be80c
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-16 22:16:47 -0300

    Programa para ver se um número é par ou ímpar

---
+++ ex008/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("------------------------------");
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O número digitado é par!!");
+            }
+            else 
+            {
+                Console.WriteLine("O número digitado é ímpar!!");
+            }
+        }
+    }
+}
