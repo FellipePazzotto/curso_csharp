43cf2b6c75a0ebbfc966631d4cc1e2deedea8540
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-21 19:32:15 -0300

    Criando um programa para identificar tipos de triângulo

---
+++ ex009/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do lado A: ");
+            int ladoA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.Write("Digite o valor do lado B: ");
+            int ladoB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.Write("Digite o valor do lado C: ");
+            int ladoC = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------");
+
+            if (ladoA < ladoB + ladoC && ladoB < ladoC + ladoA && ladoC < ladoA + ladoB)
+            {
+                Console.Write("Essas medidas formam um triângulo ");
+                if (ladoA == ladoB && ladoB == ladoC && ladoC == ladoA)
+                {
+                    Console.WriteLine("equilátero!!");
+                }
+                else if (ladoA != ladoB && ladoB != ladoC && ladoC != ladoA)
+                {
+                    Console.WriteLine("escaleno!!");
+                }
+                else
+                {
+                    Console.WriteLine("isósceles!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Com essas medidas, não é possível formar um triângulo.");
+            }
+        }
+    }
+}
