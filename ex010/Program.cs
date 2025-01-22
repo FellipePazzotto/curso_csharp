966557c7dfada9c7b4a2662f7894e6d354d2a420
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-21 21:08:08 -0300

    Criando um programa para cálculo de salário

---
+++ ex010/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ex010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu salário: R$");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            if (salario > 2500)
+            {
+                salario += (0.05 * salario);
+            }
+            else if (salario > 1250 && salario < 2500)
+            {
+                salario += (0.10 * salario);
+            }
+            else
+            {
+                salario += (0.15 * salario);
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Após o aumento, o valor do salário é R${0}.", salario);
+        }
+    }
+}
