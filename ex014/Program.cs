b584dbad93478bb7a00257e1605debd10941d828
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-21 22:11:18 -0300

    Programa para categorizar atletas

---
+++ ex014/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ex014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o ano de nascimento do atleta: ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("O atleta irá competir na categoria ");
+            if (2025 - ano <= 9)
+            {
+                Console.WriteLine("mirim.");
+            }
+            else if (2025 - ano <= 14)
+            {
+                Console.WriteLine("infantil.");
+            }
+            else if (2025 - ano <= 19)
+            {
+                Console.WriteLine("júnior.");
+            }
+            else if (2025 - ano <= 25)
+            {
+                Console.WriteLine("sênior.");
+            }
+            else
+            {
+                Console.WriteLine("master.");
+            }
+        }
+    }
+}
