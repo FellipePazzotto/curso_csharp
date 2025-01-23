1ddb53fa1372dae8f1738be51dfd7c35005f91ac
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-23 20:09:39 -0300

    Adicionando programa para somar múltiplos de três

---
+++ ex019/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            
+            for (int i = 1; i <= 500; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    total += i;
+                }
+            }
+
+            Console.WriteLine("Entre 0 e 500, os números múltiplos de 3 resultam em {0}.", total);
+        }
+    }
+}
