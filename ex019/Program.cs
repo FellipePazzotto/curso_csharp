4b00a245d476c8b144eaf97dc957e64c9eaac80b
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-23 20:16:42 -0300

    Aprimorando a soma de múltiplos de três

--- ex019/Program.cs
+++ ex019/Program.cs
@@ -5,16 +5,19 @@
         static void Main(string[] args)
         {
             int total = 0;
+            int quantidade = 0;
             
             for (int i = 1; i <= 500; i++)
             {
                 if (i % 3 == 0)
                 {
                     total += i;
+                    quantidade++;
                 }
             }
 
-            Console.WriteLine("Entre 0 e 500, os números múltiplos de 3 resultam em {0}.", total);
+            Console.WriteLine("Entre 0 e 500, há {0} números múltiplos de 3 e" +
+                " a soma deles resulta em {1}.", quantidade, total);
         }
     }
 }
