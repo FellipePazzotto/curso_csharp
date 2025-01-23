46f0dc63ab4046a6a4b9012ba3714e1bd42fdc4a
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-23 18:53:24 -0300

    Programa de descontos em formas de pagamento

---
+++ ex016/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ex016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do produto: R$");
+            double produto = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("O valor à vista, em dinheiro ou pix, é R${0}.", produto - (0.10 * produto));
+            Console.WriteLine("O valor à vista, em cartão, é R${0}.", produto - (0.05 * produto));
+            Console.WriteLine("O valor em 2 vezes no cartão é R${0}.", produto);
+            Console.WriteLine("O valor em 3 vezes, ou mais, no cartão é R${0}.", produto + (0.20 * produto));
+        }
+    }
+}
