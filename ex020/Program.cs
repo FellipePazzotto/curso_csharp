480d555277f7cbf95841b1ac084f0aa30de8e18f
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-23 20:47:36 -0300

    Programa para fazer tabuada

---
+++ ex020/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", 
+                    numero, i, numero * i);
+            }
+        }
+    }
+}
