0194ec73f81f71838bf49eeafe4de5dc357db6b6
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-23 21:18:57 -0300

    Programa para realizar progressão aritmética

---
+++ ex022/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ex022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1° termo da PA: ");
+            int termo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------------------------------------------------------------------");
+
+            Console.Write("Digite a razão da PA: ");
+            int razao  = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------------------------------------------------------------------");
+
+            Console.WriteLine("Os 10 primeiros termos dessa PA são:");
+
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.Write("{0}, ", termo += razao);
+            }
+
+            Console.WriteLine("e assim por diante.");
+        }
+    }
+}
