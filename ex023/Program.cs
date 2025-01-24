6e684b593a70a993e972489d45abb7559cc86ef2
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-23 21:33:26 -0300

    Adicionando identificação de gênero

---
+++ ex023/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ex023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o gênero do indivíduo: ");
+            string genero = Console.ReadLine().ToUpper();
+            Console.WriteLine("----------------------------------------");
+
+            while (genero != "F" && genero != "M")
+            {
+                Console.Write("Digite o gênero do indivíduo: ");
+                genero = Console.ReadLine().ToUpper();
+                Console.WriteLine("----------------------------------------");
+            }
+
+            if (genero == "F")
+            {
+                Console.WriteLine("Seja bem vinda!!");
+            }
+            else
+            {
+                Console.WriteLine("Seja bem vindo!!");
+            }
+        }
+    }
+}
