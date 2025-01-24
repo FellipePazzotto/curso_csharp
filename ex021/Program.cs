fc4d3a1d224156bba6c9df78a66f17380de86369
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-23 21:10:54 -0300

    Criando um programa para somar números pares

---
+++ ex021/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ex021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = [];
+            int soma = 0;
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.Write("Insira o {0}° número: ", i + 1);
+                int numero = Convert.ToInt32(Console.ReadLine());
+                lista.Add(numero);
+                Console.WriteLine("------------------------------------------------------------");
+            }
+
+            foreach (var number in lista)
+            {
+                if (number % 2 == 0)
+                {
+                    soma += number;
+                }
+            }
+
+            Console.WriteLine("A soma entre os números pares digitados resulta em {0}.", soma);
+        }
+    }
+}
