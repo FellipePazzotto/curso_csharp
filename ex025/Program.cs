48f310a3bd62ca0e0b0fe23e286f22462a9f7d06
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-01-28 18:55:38 -0300

    Programa de repetição

---
+++ ex025/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ex025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 3;
+
+            do
+            {
+                numero++;
+                Console.WriteLine("A");
+            } while (numero < 5);
+        }
+    }
+}
