7707ad337745876c90da634d64d5863a466595ef
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-02-04 22:13:33 -0300

    Programa para realizar fatorial de um número

---
+++ fatorial/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva um número para ser fatorado: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Você gostaria de ver cálculo (S/N)?? ");
+            string resposta = Console.ReadLine().ToLower();
+
+            int resultado = 1;
+            Console.WriteLine("-------------------------------------------------------");
+
+            if (resposta == "s")
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    resultado *= i;
+
+                    if (i < numero)
+                    {
+                        Console.Write($"{i} x ");
+                    }
+                    else
+                    {
+                        Console.Write($"{i} = ");
+                    }
+                }
+
+                Console.Write(resultado);
+            }
+            else if (resposta == "n")
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    resultado *= i;
+                }
+                
+                Console.Write(resultado);
+            }
+            else
+            {
+                Console.WriteLine("Você não selecionou uma resposta válida.");
+            }
+        }
+    }
+}
