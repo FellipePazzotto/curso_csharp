7c9d804bee65c3d41d7338e11b931758670814bf
Pazzotto <fellipe.cpazzotto@senacsp.edu.br>
2025-02-06 19:25:43 -0300

    Programa para mostrar medidas

---
+++ ex026/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ex026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva um medida em metros: ");
+            double medida = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine($"A medida em centímetros é {medida * 100} cm.");
+            Console.WriteLine($"A medida em milímetros é {medida * 1000} ml.");
+        }
+    }
+}
